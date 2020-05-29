fccf2bc8a5234a692a1a56fed9477b57108a7be3
Charles Advincula <chadvincula7636428@gmail.com>
2020-05-29 15:21:26 -0700

    Material Swapper script works
    - should probably make more materials that use the same texture, but different shader

---
+++ Censorship Shader/Assets/Scripts/MaterialSwapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwapper : MonoBehaviour
+{
+    [SerializeField]
+    private Material[] materials;
+    private int materialIndex = 0;
+
+    private Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material = materials[materialIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            ChangeMaterial();
+        }
+    }
+
+    private void ChangeMaterial()
+    {
+        materialIndex++;
+        if(materialIndex >= materials.Length)
+        {
+            materialIndex = 0;
+        }
+        renderer.material = materials[materialIndex];
+    }
+}
