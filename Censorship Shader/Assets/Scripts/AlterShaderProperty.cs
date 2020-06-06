a0da33e8ed4c8333cc81c63c2f032dc94ebab2a1
Charles Advincula <chadvincula7636428@gmail.com>
2020-06-05 23:31:45 -0700

    Got a slider to manipulate the radius property of the box slider

---
+++ Censorship Shader/Assets/Scripts/AlterShaderProperty.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlterShaderProperty : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer[] rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void SetProperty(float newValue)
+    {
+        foreach (var r in rend)
+        {
+            r.material.SetFloat("_Radius", newValue);
+        }
+    }
+}
