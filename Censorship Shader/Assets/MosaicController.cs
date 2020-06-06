7e4b6c05b16a8297f0d8a76e5c47d708e2416deb
axu11 <50363222+axu11@users.noreply.github.com>
2020-06-05 23:33:56 -0700

    tried adding sliders

---
+++ Censorship Shader/Assets/MosaicController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MosaicController : MonoBehaviour
+{
+    Renderer rend;
+    Transform tran;
+    public Slider mainSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Custom/NewSurfaceShader");
+        tran = GetComponent<Transform>();
+        Debug.Log(rend.material.shader.ToString());
+    }
+
+    // Update is called once per frame
+    public void UpdateObject()
+    {
+        Debug.Log(mainSlider.value);
+        Debug.Log(rend.materials[0].color);
+        for(int i = 0; i < rend.materials.Length; i++)
+        {
+            rend.materials[i].SetVector("_Color", new Vector4(1, 1, 1, mainSlider.value));
+            //tran.position = new Vector3(0, 0, mainSlider.value);
+        }
+        //Debug.Log(tran.position);
+
+    }
+}
